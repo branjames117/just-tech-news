925d03a66f00800553a2c254762f39f759951c3f
Brandon Hoskins <branjames117@gmail.com>
2022-01-05 21:00:53 -0600

    finished comment model and routing

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 // get all posts
 router.get('/', (req, res) => {
@@ -19,7 +19,17 @@ router.get('/', (req, res) => {
       ],
     ],
     order: [['created_at', 'DESC']],
-    include: [{ model: User, attributes: ['username'] }],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
+      { model: User, attributes: ['username'] },
+    ],
   })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => {
@@ -47,6 +57,14 @@ router.get('/:id', (req, res) => {
       ],
     ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username'],
+        },
+      },
       {
         model: User,
         attributes: ['username'],
