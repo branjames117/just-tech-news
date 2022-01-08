95c9a1e33954b83cb91e89cda4dbd3cf394c2606
Brandon Hoskins <branjames117@gmail.com>
2022-01-07 23:29:03 -0600

    added user auth

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitiated: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
 const app = express();
+app.use(session(sess));
 const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({});
